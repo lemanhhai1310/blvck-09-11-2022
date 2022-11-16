0a8ba54e1453b0824b238180dffb41663141eb50
hailm <hai.lemanh.1310@gmail.com>
2022-11-16 16:31:24 +0700

    update

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
             <img class="uk-cover" src="images/lifestyle05-desktop_2100x.jpg" alt="" uk-cover>
             <div class="uk-position-cover uk-flex uk-flex-middle">
                 <div class="uk-section-xsmall uk-width-1-1 uk-text-center">
-                    <div class="uk-container">
+                    <div class="uk-container uk-container-expand">
                         <div class="home__slideshow__txt">READY-TO-WEAR</div>
                         <h2 class="uk-h2 home__slideshow__title">Winter Sale</h2>
                         <div class="uk-child-width-auto uk-flex-center uk-grid-10 uk-grid-40-m home__slideshow__item-60-35" uk-grid>
@@ -28,7 +28,7 @@
             <img class="uk-cover" src="images/Home_8060a3f8-beff-4f61-ba4b-6c4602cefc55.jpg" alt="" uk-cover>
             <div class="uk-position-cover uk-flex uk-flex-middle">
                 <div class="uk-section-xsmall uk-width-1-1 uk-text-center">
-                    <div class="uk-container">
+                    <div class="uk-container uk-container-expand">
                         <div class="home__slideshow__txt">Blvck Home</div>
                         <h2 class="uk-h2 home__slideshow__title">Bring the lifestyle home.</h2>
                         <div class="uk-child-width-auto uk-flex-center uk-grid-10 uk-grid-40-m home__slideshow__item-60-35" uk-grid>
@@ -46,7 +46,54 @@
     <a class="uk-position-center-right uk-position-small" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
 
 </div>
-<div class="uk-height-viewport">
-
+<!--BLVCK HOME-->
+<div class="uk-height-viewport uk-flex uk-flex-middle uk-background-norepeat uk-background-center-center uk-background-cover" data-src="images/social_share_74_d4088aec-1ac0-4f07-a1b7-0ceb116e0577.jpg" uk-img>
+    <div class="uk-section-xsmall uk-width-1-1 uk-text-center">
+        <div class="uk-container uk-container-expand">
+            <div class="home__slideshow__txt">BLVCK HOME</div>
+            <h2 class="uk-h2 home__slideshow__title">All shades of black available.</h2>
+            <div class="uk-child-width-auto uk-flex-center uk-grid-10 uk-grid-40-m home__slideshow__item-60-35" uk-grid>
+                <div>
+                    <a href="" class="home__slideshow__btn uk-button uk-button-large uk-button-secondary">SHOP THE COLLECTION</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<!--/BLVCK HOME-->
+
+<!--WHTE-->
+<div class="uk-height-viewport uk-flex uk-flex-middle uk-background-norepeat uk-background-center-center uk-background-cover" data-src="images/whte.jpg" uk-img>
+    <div class="uk-section-xsmall uk-width-1-1 uk-text-center">
+        <div class="uk-container uk-container-expand">
+            <div class="home__slideshow__txt">WHTE</div>
+            <h2 class="uk-h2 home__slideshow__title">Welcome to the bright side.</h2>
+            <div class="uk-child-width-auto uk-flex-center uk-grid-10 uk-grid-40-m home__slideshow__item-60-35" uk-grid>
+                <div>
+                    <a href="" class="home__slideshow__btn uk-button uk-button-large uk-button-secondary">SHOP MEN</a>
+                </div>
+                <div>
+                    <a href="" class="home__slideshow__btn uk-button uk-button-large uk-button-secondary">SHOP WOMEN</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/WHTE-->
+
+<!--BLVCK X CASETIFY-->
+<div class="uk-height-viewport uk-flex uk-flex-middle uk-background-norepeat uk-background-center-center uk-background-cover" data-src="images/casetify_6f1d1ac2-f7b4-450c-9a99-ebe7a5e62b6b.jpg" uk-img>
+    <div class="uk-section-xsmall uk-width-1-1 uk-text-center">
+        <div class="uk-container uk-container-expand">
+            <div class="home__slideshow__txt">BLVCK X CASETIFY</div>
+            <h2 class="uk-h2 home__slideshow__title">Limited collab.</h2>
+            <div class="uk-child-width-auto uk-flex-center uk-grid-10 uk-grid-40-m home__slideshow__item-60-35" uk-grid>
+                <div>
+                    <a href="" class="home__slideshow__btn uk-button uk-button-large uk-button-secondary">Shop The Collab</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/BLVCK X CASETIFY-->
 <?php require "template-parts/layouts/footer.php"; ?>
